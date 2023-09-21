e64d4fefe50af1bfd5ac343564a97eec19dcb821
Ghader <gharibyan.ghader@gmail.com>
2023-09-22 01:52:24 +0200

    fixing workspace config

--- index.ts
+++ index.ts
@@ -1,4 +1,8 @@
 // const res = await fetch("https://deno.com");
 // const body = await res.text();
+
+import { multiply } from "./utils.ts";
+
+
 // console.log(body);
-console.log("%cHello World", "color: red");
+console.log('%cHello World' + multiply(15, 8), 'color: red');
