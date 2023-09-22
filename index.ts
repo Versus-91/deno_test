b5a290fa70bf3c0f5ff05298065ba2670dd1a451
Ghader <gharibyan.ghader@gmail.com>
2023-09-22 20:24:08 +0200

    ddeno siomple response

--- index.ts
+++ index.ts
@@ -1,8 +1,11 @@
 // const res = await fetch("https://deno.com");
 // const body = await res.text();
 
-import { multiply } from "./utils.ts";
-
+import { multiply } from './utils.ts';
 
 // console.log(body);
 console.log('%cHello World' + multiply(15, 8), 'color: red');
+function handler(_req: Request): Response {
+  return new Response('Hello, World!');
+}
+Deno.serve(handler);
